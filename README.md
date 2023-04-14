a82b63265976e0109ecb394c5853c1ff0ae83a85
Jannis Spiekermann <janniss@hotmail.de>
2023-04-14 13:41:35 +0200

    Separate notebooks for waveform gen and fourier analysis

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ An repo that contains different Speech Processing projects and applications.
 ## Notebooks and Visualization
 
 The code written for speech analysis, synthesis and recognition can be tested using the jupyter notebooks in the `notebooks` directory.
+
+The follwing notebooks currently exist:
+
+- Waveform generation
+- Fourier Analysis
--- tests/test_fourier.py
+++ tests/test_fourier.py
@@ -4,17 +4,11 @@ from speedypanther.analysis.fourier import dft
 import numpy as np
 
 def test_dft():
-    
-    # Create a new complex wave to play around with.
-    _, complex_wave = sample_complex_wave(frequencies=[600, 800, 1000, 1600], coeffs=[0.3, 0.2, 0.4, 0.1], end_time=0.1)
-
-    duration = 0.005
-    sample_rate = 44_100
-    # Take a window of samples out of the complex wave.
-    sample_window = complex_wave[:int(duration * sample_rate)]
+    # Create a new complex wave with a typical short term window length of 0.005 s.
+    _, sample_window = sample_complex_wave(frequencies=[600, 800, 1000, 1600], coeffs=[0.3, 0.2, 0.4, 0.1], end_time=0.005)
 
     # Turn the time representation of the wave into a frequency representation.
     freq_ampls = dft(sample_window)
-
     expected = np.fft.fft(sample_window)
+
     assert np.allclose(freq_ampls, expected)
